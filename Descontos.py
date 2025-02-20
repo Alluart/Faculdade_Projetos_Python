6b04849bea68c4566c447ab880993f35ae48e498
thiago <thiagoagostinho6@gmail.com>
2025-02-20 20:32:23 -0300

    Desconto

---
+++ Descontos.py
@@ -0,0 +1,54 @@
+# Elabore um programa em Python que: 
+# Deve-se implementar o print com uma mensagem de boas-vindas que apareça o seu nome e sobrenome [EXIGÊNCIA DE CÓDIGO 1 de 6]; 
+
+
+
+
+
+# Deve-se implementar o input do valor unitário e da quantidade do produto [EXIGÊNCIA DE CÓDIGO 2 de 6]; 
+
+
+
+
+
+
+
+# Deve-se implementar o desconto conforme a enunciado acima (obs.: atente-se as condições de menor, igual e maior) [EXIGÊNCIA DE CÓDIGO 3 de 6]; 
+
+
+
+
+
+# Deve-se implementar o valor total sem desconto e o valor total com desconto [EXIGÊNCIA DE CÓDIGO 4 de 6]; 
+
+
+
+
+
+# Deve-se implementar as estruturas if, elif e else (todas elas) [EXIGÊNCIA DE CÓDIGO 5 de 6];  
+
+
+
+
+
+
+
+# Deve-se inserir comentários relevantes no código [EXIGÊNCIA DE CÓDIGO 6 de 6]; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Deve-se apresentar na saída de console uma mensagem de boas-vindas com seu nome [EXIGÊNCIA DE SAÍDA DE CONSOLE 1 de 2]; 
+# Deve-se apresentar na saída de console um pedido recebendo desconto (valor total sem desconto maior ou igual a 2500) [EXIGÊNCIA DE SAÍDA DE CONSOLE 2 de 2];   
