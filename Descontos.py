6684116b709eb82381237feeeb25a881f6ff1029
thiago <thiagoagostinho6@gmail.com>
2025-02-25 00:50:44 -0300

    Finalizando primeiro trabalho

--- Descontos.py
+++ Descontos.py
@@ -1,54 +1,17 @@
-# Elabore um programa em Python que: 
-# Deve-se implementar o print com uma mensagem de boas-vindas que apareça o seu nome e sobrenome [EXIGÊNCIA DE CÓDIGO 1 de 6]; 
-
-
-
-
-
-# Deve-se implementar o input do valor unitário e da quantidade do produto [EXIGÊNCIA DE CÓDIGO 2 de 6]; 
-
-
-
-
-
-
-
-# Deve-se implementar o desconto conforme a enunciado acima (obs.: atente-se as condições de menor, igual e maior) [EXIGÊNCIA DE CÓDIGO 3 de 6]; 
-
-
-
-
-
-# Deve-se implementar o valor total sem desconto e o valor total com desconto [EXIGÊNCIA DE CÓDIGO 4 de 6]; 
-
-
-
-
-
-# Deve-se implementar as estruturas if, elif e else (todas elas) [EXIGÊNCIA DE CÓDIGO 5 de 6];  
-
-
-
-
-
-
-
-# Deve-se inserir comentários relevantes no código [EXIGÊNCIA DE CÓDIGO 6 de 6]; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Deve-se apresentar na saída de console uma mensagem de boas-vindas com seu nome [EXIGÊNCIA DE SAÍDA DE CONSOLE 1 de 2]; 
-# Deve-se apresentar na saída de console um pedido recebendo desconto (valor total sem desconto maior ou igual a 2500) [EXIGÊNCIA DE SAÍDA DE CONSOLE 2 de 2];   
+# Mensagem inicial
+print('Bem-vindo ao aplicativo de vendas de Thiago Agostinho')
+quantidadeProdutos = float(input('Qual a quantidade do produto x você gostaria de comprar?'))
+valorProduto = float(input('Qual o valor do produto x?'))
+# calculo do valor
+valorSemDesconto = quantidadeProdutos * valorProduto
+# calculo dos descontos
+if(valorSemDesconto >= 2500 and valorSemDesconto < 6000):
+    valorFinal = valorSemDesconto - (valorSemDesconto * 0.04)
+elif(valorSemDesconto >= 6000 and valorSemDesconto < 10000):
+    valorFinal = valorSemDesconto - (valorSemDesconto * 0.07)
+elif(valorSemDesconto >= 10000):
+    valorFinal = valorSemDesconto - (valorSemDesconto * 0.11)
+else:
+    valorFinal = valorSemDesconto
+#Mensagem final da compra
+print(f'O valor sem desconto foi de R${valorSemDesconto:.2f} e o valor com o desconto foi de R${valorFinal:.2f}')
